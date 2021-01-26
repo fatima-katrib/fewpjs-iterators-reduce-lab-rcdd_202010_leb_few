0f52faf894ec7d3457ce793d2e450d261752a9d2
jovial-application-1943 <fatima.katrib@lau.edu>
2021-01-26 21:29:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
 function getTotalBatteries(batteryBatches){ 
-  let totalBatteries =batteryBatches.reduce((total, batch) => total + batch, 0);
+  let totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
 }
